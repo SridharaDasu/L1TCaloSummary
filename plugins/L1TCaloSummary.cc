6066b9b48443363400f6d6fb76c4b78f9a0364ac
Sridhara Dasu <dasu@hep.wisc.edu>
2016-04-27 01:38:17 +0200

    Added pum lut - not using it yet

--- plugins/L1TCaloSummary.cc
+++ plugins/L1TCaloSummary.cc
@@ -94,6 +94,8 @@ private:
   std::vector< std::vector< std::vector < uint32_t > > > hcalLUT;
   std::vector< std::vector< uint32_t > > hfLUT;
 
+  std::vector< std::vector< uint32_t > > pumLUT;
+
   std::vector< UCTTower* > twrList;
 
   bool useLSB;
@@ -127,6 +129,7 @@ L1TCaloSummary::L1TCaloSummary(const edm::ParameterSet& iConfig) :
   ecalLUT(28, std::vector< std::vector<uint32_t> >(2, std::vector<uint32_t>(256))),
   hcalLUT(28, std::vector< std::vector<uint32_t> >(2, std::vector<uint32_t>(256))),
   hfLUT(12, std::vector < uint32_t >(256)),
+  pumLUT(18, std::vector < uint32_t >(22)),
   useLSB(iConfig.getParameter<bool>("useLSB")),
   useCalib(iConfig.getParameter<bool>("useCalib")),
   useECALLUT(iConfig.getParameter<bool>("useECALLUT")),
@@ -158,8 +161,29 @@ L1TCaloSummary::L1TCaloSummary(const edm::ParameterSet& iConfig) :
       }
     }
   }
+  pumLUT[ 0] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT00");
+  pumLUT[ 1] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT01");
+  pumLUT[ 2] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT02");
+  pumLUT[ 3] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT03");
+  pumLUT[ 4] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT04");
+  pumLUT[ 5] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT05");
+  pumLUT[ 6] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT06");
+  pumLUT[ 7] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT07");
+  pumLUT[ 8] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT08");
+  pumLUT[ 9] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT09");
+  pumLUT[10] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT10");
+  pumLUT[11] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT11");
+  pumLUT[12] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT12");
+  pumLUT[13] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT13");
+  pumLUT[14] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT14");
+  pumLUT[15] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT15");
+  pumLUT[16] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT16");
+  pumLUT[17] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT17");
+  pumLUT[18] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT18");
+  pumLUT[19] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT19");
+  pumLUT[20] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT20");
+  pumLUT[21] = iConfig.getParameter<std::vector < uint32_t > >("pumLUT21");
 }
-
 
 L1TCaloSummary::~L1TCaloSummary() {
   if(layer1 != 0) delete layer1;
@@ -425,6 +449,7 @@ L1TCaloSummary::endJob() {
 }
 
 // ------------ method called when starting to processes a run  ------------
+
 void
 L1TCaloSummary::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
   if(!L1TCaloLayer1FetchLUTs(iSetup, ecalLUT, hcalLUT, hfLUT, useLSB, useCalib, useECALLUT, useHCALLUT, useHFLUT)) {
@@ -434,7 +459,7 @@ L1TCaloSummary::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
     twrList[twr]->setECALLUT(&ecalLUT);
     twrList[twr]->setHCALLUT(&hcalLUT);
     twrList[twr]->setHFLUT(&hfLUT);
-    }
+  }
 }
  
 // ------------ method called when ending the processing of a run  ------------
